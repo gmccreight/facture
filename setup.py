250bfa2b8ebdb92f1f57caf97c1601c8b76e43c9
Gordon McCreight <gordon@mccreight.com>
2019-04-13 11:46:52 -0700

    ensure README is added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Facture',
-    version='0.1.3',
+    version='0.1.4',
     author='Gordon McCreight',
     author_email='gordon@mccreight.com',
     packages=['facture'],
