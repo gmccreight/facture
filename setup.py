5c1921027ecc15e05e3ca247b18862aa14df5bd8
Gordon McCreight <gordon@mccreight.com>
2019-12-17 13:32:14 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='facturedata',
-    version='0.1.12',
+    version='0.1.13',
     author='Gordon McCreight',
     author_email='gordon@mccreight.com',
     packages=['facturedata'],
