ed7877bb794301e72a596345316be1d142dd6a5b
Gordon McCreight <gordon@mccreight.com>
2020-01-08 10:08:49 -0800

    version bump to 0.2
    There are not breaking changes, but there is significant new functionality, not just tweaks, hence the minor version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='facturedata',
-    version='0.1.13',
+    version='0.2.01',
     author='Gordon McCreight',
     author_email='gordon@mccreight.com',
     packages=['facturedata'],
